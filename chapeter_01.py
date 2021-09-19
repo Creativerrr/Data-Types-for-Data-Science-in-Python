b9fa80a90cb8ddee7d94a7d37e28291610a96199
Bold Batdorj <43408911+Creativerrr@users.noreply.github.com>
2021-09-19 22:06:34 +1000

    chapter1

--- chapeter_01.py
+++ chapeter_01.py
@@ -36,4 +36,64 @@ for name in sorted(baby_names):
     # Print each name
     print(name)
 
-#1.3 Loop over records 
+#1.3 Data type usage
+
+#Which data type would you use if you wanted your data to be immutable and ordered?
+#Answer: Typle
+
+#1.4 Using and unpacking tuples
+
+# Pair up the girl and boy names: pairs
+pairs = zip(girl_names, boy_names)
+
+# Iterate over pairs
+for idx, pair in enumerate(pairs):
+    # Unpack pair: girl_name, boy_name
+    girl_name, boy_name = pair
+    # Print the rank and names associated with each rank
+    print('Rank {}: {} and {}'.format(idx,girl_name, boy_name))
+
+#1.5 Making tuples by accident
+
+# Create the normal variable: normal
+normal = 'simple'
+
+# Create the mistaken variable: error
+error = 'trailing comma',
+
+# Print the types of the variables
+print(type(normal))
+print(type(error))
+
+#1.6 Finding all the data and the overlapping data between sets
+
+# Find the union: all_names
+all_names = baby_names_2011.union(baby_names_2014)
+
+# Print the count of names in all_names
+print(len(all_names))
+
+# Find the intersection: overlapping_names
+overlapping_names = baby_names_2011.intersection(baby_names_2014)
+
+# Print the count of names in overlapping_names
+print(len(overlapping_names))
+
+#1.7 Determining set differences
+
+# Create the empty set: baby_names_2011
+baby_names_2011 = set()
+
+# Loop over records and add the names from 2011 to the baby_names_2011 set
+for row in records:
+    # Check if the first column is '2011'
+    if row[0] == '2011':
+        # Add the fourth column to the set
+        baby_names_2011.add(row[3])
+
+# Find the difference between 2011 and 2014: differences
+differences = baby_names_2011.difference(baby_names_2014)
+
+# Print the differences
+print(differences)
+
