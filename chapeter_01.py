836d97d20fcfa41503fee9c3627f108d8f46da5d
Bold Batdorj <43408911+Creativerrr@users.noreply.github.com>
2021-09-19 21:00:27 +1000

    chapter1

---
+++ chapeter_01.py
@@ -0,0 +1,32 @@
+#Chapter 1 - Fundamental data types
+
+
+#1.1 
+# Create a list containing the names: baby_names
+baby_names = ['Ximena', 'Aliza', 'Ayden', 'Calvin']
+
+# Extend baby_names with 'Rowen' and 'Sandeep'
+new = ['Rowen','Sandeep']
+baby_names.extend(new)
+
+# Print baby_names
+print(baby_names)
+
+# Find the position of 'Aliza': position
+position = baby_names.index('Aliza')
+
+# Remove 'Aliza' from baby_names
+baby_names.pop(1)
+
+# Print baby_names
+print(baby_names)
+
+#1.2 Loop over records 
+for ____ in ____:
+    # Add the name to the list
+    ____
+    
+# Sort the names in alphabetical order
+for name in ____:
+    # Print each name
+    print(name)
